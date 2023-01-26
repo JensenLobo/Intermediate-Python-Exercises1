13401ffd64601bd31e0bd0b9b5f300b400804533
jensen <jensenlobo30@outlook.com>
2023-01-25 22:26:09 -0500

    exercise_5 done

--- exercise_05.py
+++ exercise_05.py
@@ -4,17 +4,18 @@ class BankAccount:
         person.bal = balance
     
     def deposit(person, amount):
-        person.bal += amount
+        person.bal += amount #adds the amount to the person's balance
         return person.bal
     
     def withdraw(person, amount):
-        person.bal -= amount
+        person.bal -= amount #subtracts the amount from the balance
         return person.bal
     
     def get_balance(person):
-        return '{} has a balance of {}'.format(person.account_name, person.bal)
+        return '{} has a balance of {}'.format(person.account_name, person.bal) 
+        #the format put the variables in the {} respectively per parameter
 
-my_account = BankAccount('Jensen Lobo', 70.16)
+my_account = BankAccount('Jensen Lobo', 70.16) #hardcoded information, making an instance of the BankAccount Class
 my_account.deposit(50)
 my_account.withdraw(10.16)
-print(my_account.get_balance())
+print(my_account.get_balance()) 
