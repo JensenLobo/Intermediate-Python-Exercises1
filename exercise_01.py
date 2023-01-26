be08af223f4cba8ee9f99a3fcff245c334065565
jensen <jensenlobo30@outlook.com>
2023-01-25 22:28:30 -0500

    exercise_1 done

--- exercise_01.py
+++ exercise_01.py
@@ -1,9 +1,9 @@
 def show(list):
     b = []
     for i in list:
-        if i not in b: 
-            b.append(i)
-    return b
+        if i not in b: #checks to see if list[i] has been added to the b list
+            b.append(i) #only adds the unique characters/integers
+    return b 
         
 my_list = [1,2,3,2,1,4]
 unique_list = show(my_list)
